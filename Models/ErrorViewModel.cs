d07c7957dfe579dce5d86fc0b2b8db4e2281ddde
itsdoanguen <doannguyennguyen654@gmail.com>
2025-04-08 12:41:41 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotNET_courseproject_CourseRegister.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
